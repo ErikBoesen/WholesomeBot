bb369bd2d841e8833c826c148b590f17ae46fe6a
ErikBoesen <me@erikboesen.com>
2020-09-19 00:36:34 -0600

    Add initial char constants

---
+++ chars.rb
@@ -0,0 +1,13 @@
+class Chars
+  FISH_TYPES = ["🐟", "🐡", "🐠"]
+  RARE_SWIMMER_TYPES = ["🐙", "🐬", "🦑", "🦈"]
+  PLANT_TYPES = ["🌱", "🌾", "🌿"]
+  RARE_BOTTOM_DWELLERS = ["🐌", "🏰", "🦀", "🐚", "⚓️", "☘️"]
+  EXCEEDINGLY_RARE_JUNK = ["🎱", "🎲", "🎮", "🗿"]
+  IDEOGRAPHIC_SPACE = "\u3000"
+  EN_SPACE = "\u2002"
+  EM_SPACE = "\u2003"
+  THREE_PER_EM_SPACE = "\u2004"
+  THIN_SPACE = "\u2009"
+  HAIR_SPACE = "\u200a"
+end
