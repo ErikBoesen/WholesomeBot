420f741f285e8dc64e3b4f38cece5bb8aadbbbdd
ErikBoesen <me@erikboesen.com>
2020-09-19 01:33:54 -0600

    Read from JSON file

--- bot.rb
+++ bot.rb
@@ -1,12 +1,14 @@
+require 'json'
 require 'twitter'
 require './generator'
 
+credentials = JSON.parse(File.read("credentials.json"))
 
 client = Twitter::REST::Client.new do |config|
-    config.consumer_key = ENV['CONSUMER_KEY']
-    config.consumer_secret = ENV['CONSUMER_SECRET']
-    config.access_token = ENV['ACCESS_TOKEN']
-    config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    config.consumer_key = credentials['consumer_key']
+    config.consumer_secret = credentials['consumer_secret']
+    config.access_token = credentials['access_token']
+    config.access_token_secret = credentials['access_token_secret']
 end
 
 def tweet(text)
