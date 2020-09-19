db2e63a8f44e4bfdbc592d1f6afdd909454ae492
ErikBoesen <me@erikboesen.com>
2020-09-19 00:55:19 -0600

    Add wholesome chars

--- chars.rb
+++ chars.rb
@@ -1,9 +1,6 @@
 class Chars
-  FISH_TYPES = ["🐟", "🐡", "🐠"]
-  RARE_SWIMMER_TYPES = ["🐙", "🐬", "🦑", "🦈"]
-  PLANT_TYPES = ["🌱", "🌾", "🌿"]
-  RARE_BOTTOM_DWELLERS = ["🐌", "🏰", "🦀", "🐚", "⚓️", "☘️"]
-  EXCEEDINGLY_RARE_JUNK = ["🎱", "🎲", "🎮", "🗿"]
+  FACES = ["🥰", "😊", "☺️", "🥺"]
+  RARE_DECORATORS = ["✨", "💫", "🌈", "☀️", "💖", "💕", "💓", "💞", "❤️"]
   IDEOGRAPHIC_SPACE = "\u3000"
   EN_SPACE = "\u2002"
   EM_SPACE = "\u2003"
